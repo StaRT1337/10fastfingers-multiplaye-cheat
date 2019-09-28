2cdb16d7a4a193de51e330dd6eaf5d13e81e0d16
Tom <tomking1902@gmail.com>
2019-09-29 02:54:00 +0300

    init

---
+++ cheat.js
@@ -0,0 +1,16 @@
+/*
+    10fastfingers multiplayer cheat.
+    only works on 10ff.net and not on the conventional website.
+    copy the code into the console only once the wait time finishes.
+*/
+
+const highlight = document.getElementsByClassName("highlight");
+const input = document.getElementsByTagName("input")[0];
+const event = new InputEvent("input", {data: "k"});
+
+setInterval(() => {
+    if (highlight.length > 0) {
+        input.value = highlight[0].innerText + " ";
+        input.dispatchEvent(event)
+    }
+}, 100);
